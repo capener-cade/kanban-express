23aeee0f62a7537f9325dc0a9e37d812ec5c68e7
Colin Marr <colinm@ifit.com>
2021-03-11 15:17:11 -0500

    fix: move mongodb to docker

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-import 'dotenv/config';
-import Card from "./model/card"
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import "dotenv/config";
+import Card from "./model/card";
 
 const port = 3001;
 
@@ -14,29 +14,29 @@ app.use(bodyParser.json());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
 
   console.log(`${req.method} ${req.originalUrl}`);
   next();
 });
 
-mongoose.connect(process.env.MONGO_CONNECTION_STRING!,
-  { useUnifiedTopology: true, useNewUrlParser: true },
-  () => {
-    console.log('Connected to the DB');
-  });
+//TODO:move back to .env file
+mongoose.connect("mongodb://localhost:27018/kanban", { useUnifiedTopology: true, useNewUrlParser: true }, (err) => {
+  if (err) console.log("oh no", err);
+  console.log("Connected to the DB");
+});
 
-app.get('/', (req, res) => res.send('success'));
-app.get('/api/ping', (req, res) => res.send('success'));
+app.get("/", (req, res) => res.send("success"));
+app.get("/api/ping", (req, res) => res.send("success"));
 // grab a board from the db.
-app.get('/api/board/:id/cards', async (req, res) => {
+app.get("/api/board/:id/cards", async (req, res) => {
   const cards = await Card.find({ boardId: req.params.id });
   res.send(cards);
 });
 
-app.post('/api/board/:id/cards', async (req, res) => {
+app.post("/api/board/:id/cards", async (req, res) => {
   // validate the data that goes into the db.
   try {
     const newCard = new Card(req.body);
@@ -47,7 +47,7 @@ app.post('/api/board/:id/cards', async (req, res) => {
   }
 });
 
-app.delete('/api/board/:boardId/:cardId', async (req, res) => {
+app.delete("/api/board/:boardId/:cardId", async (req, res) => {
   try {
     console.log(req.params.boardId);
     console.log(req.params.cardId);
@@ -58,8 +58,7 @@ app.delete('/api/board/:boardId/:cardId', async (req, res) => {
   }
 });
 
-app.listen(port)
-  .once('listening', () => {
-    // eslint-disable-next-line no-console
-    console.log(`listening on port ${port}`);
-  });
+app.listen(port).once("listening", () => {
+  // eslint-disable-next-line no-console
+  console.log(`listening on port ${port}`);
+});
