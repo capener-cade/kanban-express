c26ad35bfca46a8d16e5e58226186865c25e6b27
capener-cade <cadec@ifit.com>
2021-05-27 09:07:04 -0600

    Co-authored-by: Colin Marr <ifit-colin@users.noreply.github.com>

--- src/controllers/board.test.ts
+++ src/controllers/board.test.ts
@@ -1,8 +1,13 @@
 import boardController from "./board";
 import boardModel from "../model/board";
+import { Types } from "mongoose";
 
 describe("board controller", () => {
   const newBoard = { title: "new board" };
+  let id: any;
+  beforeEach(() => {
+    id = Types.ObjectId();
+  });
 
   describe("create board", () => {
     beforeEach(() => {
@@ -51,4 +56,74 @@ describe("board controller", () => {
       expect(response).toStrictEqual([newBoard, newBoard]);
     });
   });
+
+  describe("get a single board", () => {
+    beforeEach(() => {
+      jest.spyOn(boardModel, "findOne").mockImplementation(() => newBoard as any);
+    });
+
+    afterEach(() => {
+      jest.restoreAllMocks();
+    });
+
+    it("should be a function", () => {
+      expect(typeof boardController.getSingleBoard === "function").toBeTruthy();
+    });
+
+    it("should search the database for a board", async () => {
+      await boardController.getSingleBoard(id);
+      expect(boardModel.findOne).toHaveBeenCalledWith({ _id: id });
+    });
+
+    it("should return a single board", async () => {
+      const response = await boardController.getSingleBoard(id);
+      expect(response).toStrictEqual(newBoard);
+    });
+  });
+
+  describe("update a board", () => {
+    beforeEach(() => {
+      jest.spyOn(boardModel, "updateOne").mockImplementation(() => newBoard as any);
+      boardModel.prototype.save = jest.fn();
+    });
+
+    afterEach(() => {
+      jest.restoreAllMocks();
+    });
+
+    it("should be a function", () => {
+      expect(typeof boardController.updateBoard === "function").toBeTruthy();
+    });
+
+    it("should call updateOne", async () => {
+      const newBoardTitle = "newlyUpdatedBoard";
+      await boardController.updateBoard(id, newBoardTitle);
+      expect(boardModel.updateOne).toHaveBeenCalledWith({}, { $set: { title: newBoardTitle } });
+    });
+
+    it("should return the updated the board", async () => {
+      const newBoardTitle = "newlyUpdatedBoard";
+      const updatedBoard = await boardController.updateBoard(id, newBoardTitle);
+      console.log("test updatedBoard", updatedBoard);
+      expect(updatedBoard).toEqual({ title: "newlyUpdatedBoard" });
+    });
+
+    // describe("when the board does not exist", () => {
+    //   beforeEach(() => {
+    //     jest.spyOn(boardModel, "findOne").mockImplementation(() => undefined);
+    //   });
+
+    //   afterEach(() => {
+    //     jest.restoreAllMocks();
+    //   });
+
+    //   it("should throw an error", async () => {
+    //     await expect(boardController.updateBoard(id, { title: "no board" })).rejects.toThrow();
+    //   });
+    // });
+  });
+
+  // describe("delete a board", () => {
+  //   console.log("delete was hit");
+  // });
 });
--- src/controllers/board.ts
+++ src/controllers/board.ts
@@ -1,4 +1,9 @@
+import { FilterQuery, Document, Types, UpdateQuery } from "mongoose";
 import boardModel from "../model/board";
+
+interface IBoard {
+  title: string;
+}
 
 const createBoard = async (req: any) => {
   const newBoard = new boardModel(req.body);
@@ -11,7 +16,26 @@ const getBoards = async () => {
   return boardList;
 };
 
+const getSingleBoard = async (id: Types.ObjectId) => {
+  const board = await boardModel.findOne({ _id: id });
+  return board;
+};
+
+const updateBoard = async (id: Types.ObjectId, newBoardTitle: any) => {
+  const board = await boardModel.updateOne({}, { $set: { title: newBoardTitle } });
+  if (!board) throw new Error("board does not exist");
+  console.log("controller board", board);
+  return board;
+};
+
+const deleteBoard = async (req: any) => {
+  await boardModel.deleteOne(req);
+};
+
 export default {
   createBoard,
   getBoards,
+  getSingleBoard,
+  updateBoard,
+  deleteBoard,
 };
